8c6dad0af4c603a38305ab7c20fd9f237f1afbc3
gopaltani14 <gopaltani14@gmail.com>
2025-04-22 07:30:53 -0700

    fix

--- src/Images.jsx
+++ src/Images.jsx
@@ -6,8 +6,8 @@ function Images() {
       Images
       <div>
         <img src="/20mb1.jpg" alt="img1" width={500} height={500} />
-        <img src="20mb2.jpg" alt="img2" width={500} height={500} />
-        <img src="20mb3.jpg" alt="img3" width={500} height={500} />
+        <img src="/20mb2.jpg" alt="img2" width={500} height={500} />
+        <img src="/20mb3.jpg" alt="img3" width={500} height={500} />
       </div>
     </div>
   );
