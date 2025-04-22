fdff1bd1179e133eec9c9a14d493d50dca84a4ab
gopaltani14 <gopaltani14@gmail.com>
2025-04-22 06:59:24 -0700

    images

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./Carbon.scss";
 import "./index.css";
 import App from "./App.jsx";
 import Chart from "./Chart.jsx";
