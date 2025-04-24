4d28b82a6f9cd3ff2f78cf8a23814b176669d448
gopaltani14 <gopaltani14@gmail.com>
2025-04-24 07:30:24 -0700

    fix

--- src/Images.jsx
+++ src/Images.jsx
@@ -9,11 +9,12 @@ function Images() {
           display: "flex",
           gap: "1rem",
           flexWrap: "wrap",
+          justifyContent: "space-evenly",
         }}
       >
-        <img src="/20mb1.jpg" alt="img1" width={500} height={500} />
-        <img src="/20mb2.jpg" alt="img2" width={500} height={500} />
-        <img src="/20mb3.jpg" alt="img3" width={500} height={500} />
+        <img src="/20mb1.jpg" alt="img1" width={300} height={300} />
+        <img src="/20mb2.jpg" alt="img2" width={300} height={300} />
+        <img src="/20mb3.jpg" alt="img3" width={300} height={300} />
       </div>
     </div>
   );
