31940b2e3643bb7987a42057ba2a022d13f1bedd
gopaltani14 <gopaltani14@gmail.com>
2025-04-24 07:26:21 -0700

    fix

--- src/Images.jsx
+++ src/Images.jsx
@@ -3,13 +3,15 @@ import React from "react";
 function Images() {
   return (
     <div>
-      <div style={{ marginTop: "1rem" }}>
-        <img
-          src="https://examplefile.com/image/jpg/20-mb-jpg"
-          alt="img1"
-          width={500}
-          height={500}
-        />
+      <div
+        style={{
+          marginTop: "1rem",
+          display: "flex",
+          gap: "1rem",
+          flexWrap: "wrap",
+        }}
+      >
+        <img src="/20mb1.jpg" alt="img1" width={500} height={500} />
         <img src="/20mb2.jpg" alt="img2" width={500} height={500} />
         <img src="/20mb3.jpg" alt="img3" width={500} height={500} />
       </div>
